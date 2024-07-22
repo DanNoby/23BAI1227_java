16f204a9883184415f819d6f5bc5cdb641467a26
DanNoby <134064333+DanNoby@users.noreply.github.com>
2024-07-22 18:30:14 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,51 @@
+/* 
+22-07-2024
+
+public class Main{
+    public static void main(String[] args) {
+        String firstname = "John";
+        String lastname = "Doe";
+        System.out.println(firstname+" "+lastname);
+        System.out.println(firstname.concat(lastname));     // concatenation
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String x = "10";
+        int y = 20;             
+        String z = x + y;              // string + int
+        System.out.println(z);
+    }
+}
+
+// single quote \'' , double quote \"" , backslash \\ 
+
+public class Main {
+    public static void main(String[] args) {
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        System.out.println(txt);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,10)); // Math max method
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.min(5,10)); // Math min method
+        System.out.println(Math.sqrt(16)); // Math sqrt method
+        System.out.println(Math.abs(-4.7)); // absolute value 
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.random());   //Math.random() 0.0 inclusive
+    }        //1.0 exclusive
+}
+*/
+
+
---
+++ Vars.java
@@ -0,0 +1,60 @@
+/* 
+18-07-2024
+
+public class vars2 {
+    public static void main(String[] args) {
+        double myDouble = 9.78d;
+        int myint = (int) myDouble;
+        System.out.println(myDouble);
+        System.out.println(myint);
+    }   // Narrow casting
+}
+
+public class Ops {
+    public static void main(String[] args) {
+        int sum1 = 100+50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum1 + sum2;
+        String sum4 = sum1 + "hello";
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+        System.out.println(sum4);
+    }   // addiiton using + operator
+}
+
+public class Ternary {
+    public static void main(String[] args) {
+        int x,y;
+        x = 20;
+        y = (x==1) ? 1:0;
+        System.out.println("Value of y is:" + y);
+        y = (x==20) ? 1:0;
+        System.out.println("Value of y is:" + y);
+    }   // using conditional operatorto replace if/else stmt
+}
+
+public class SSString{
+    public static void main(String[] args) {
+        String txt = "The quick brown fox jumped over the lazy dog";
+        System.out.println("The length of the txt string is:" + txt.length());
+    }   // finding the no of chars in string
+}
+
+public class Stringmethods{
+    public static void main(String[] args) {
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toUpperCase());
+    }   //converting to lower/uppercase in string
+}
+
+public class Stringmethods{
+    public static void main(String[] args) {
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate"));
+    }   //using indexOf()
+}
+*/
+
+
