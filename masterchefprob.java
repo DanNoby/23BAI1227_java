cc6d510b9cd25b9131d78b04a0e3c07e87daf4b0
DanNoby <134064333+DanNoby@users.noreply.github.com>
2024-09-22 20:55:08 +0530

    16-09-2024

---
+++ masterchefprob.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+/*
+//
+public class masterchefprob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int TT = sc.nextInt();
+        int XX = sc.nextInt();
+        int YY = sc.nextInt();
+        for(int i=0; i<TT; i++) {
+            double total = XX*YY;
+            System.out.format("%.2f\n",total);
+        }
+        sc.close();
+    }
+}
+
+//
+class Animal {
+    String color = "white";
+}
+class Dog extends Animal {
+    String color = "black";
+    void printColor() {
+        System.out.println(color);
+        System.out.println(super.color);
+    }
+}
+public class masterchefprob {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.printColor();
+    }
+}
+
+class Animal {
+    void eat() {System.out.println("eating...");}
+}
+class Dog extends Animal {
+    void eat() {System.out.println("eating bread...");}
+    void bark() {System.out.println("barking...");}
+    void work() {super.eat(); bark();}
+}
+
+public class masterchefprob {
+    public static void main(String[] args) {
+        Dog d= new Dog();
+        d.work();
+    }
+}
+
+//
+class Animal {
+    Animal() {System.out.println("animal is created");}
+}
+class Dog extends Animal {
+    Dog() {
+        super();
+        System.out.println("dog is created");
+    }
+}
+
+public class masterchefprob {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+    }
+}
+*/
+
+//syllabus for PAT3 AND 4 OVER
+//
